ce88bc2f1429574cfc60814883627f635cb31a24
Vaharsolta <vaha-22@mail.ru>
2019-08-03 19:59:52 +0300

    Added animation for Skills

--- js/script.js
+++ js/script.js
@@ -6,4 +6,25 @@ $(document).ready(function () {
 	$('.works__btns button, .works__btns a').on('blur', function (e) {
 		$(this).parents('.works__element').removeClass('works__element--active');
 	});
+
+	function moveProgressBar(node, nodeLine, tooltip, animationLength = 1500) {
+	    const progressElement = $(node);
+	    progressElement.each(function (value, item) {
+	      $(item).find(nodeLine).animate({
+	        width: item.dataset.progressPercent+'%'
+	      }, animationLength);
+	      $(item).find(tooltip).show(animationLength);
+	    });
+	  }
+
+  let animate = true;
+  $(window).scroll(function () {
+    if($('.skills').offset().top <= $(window).scrollTop() + 150) {
+      if(animate) {
+        moveProgressBar('.progress__element', '.progress__line', '.progress__tooltip');
+      }
+      animate = false;
+    }
+  });
+
 });
